7c0ec5240497d9763866e317b1bcee6417b5a0cb
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-01-11 20:44:59 +0900

    Primeiro contato com a linguagem Java

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.primeiroprograma;
+
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+    }
+}
