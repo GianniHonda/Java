fbc776bf9296b2c4478741b84ba75b330603a73b
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-01-28 08:03:39 +0900

    Testando tipos primitivos em JAVA

---
+++ TiposPrimitivos/src/main/java/com/mycompany/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tiposprimitivos;
+
+/**
+ *
+ * @author gigi_
+ */
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String nome =  "Gianni";
+        float nota = 8.5f;
+        System.out.format("A nota de %s é %.2f", nome,  nota);
+    }
+}
