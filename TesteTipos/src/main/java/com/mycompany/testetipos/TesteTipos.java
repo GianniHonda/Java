4dc09731d3de69a3457715bb578ab7ece03d2e96
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-01-28 08:30:27 +0900

    Convertendo tipos de classe em JAVA

---
+++ TesteTipos/src/main/java/com/mycompany/testetipos/TesteTipos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.testetipos;
+
+/**
+ *
+ * @author gigi_
+ */
+public class TesteTipos {
+
+    public static void main(String[] args) {
+        int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+    }
+}
