ccc0270f4c636dda7aa0d7f85f673a79d9f8ada7
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-02-07 20:48:56 +0900

    Praticando classe Math em JAVA
    Prática de operadores aritméticos, com diversos mini códigos em JAVA utilizando a classe Math.

--- src/main/java/com/mycompany/operadoresaritmeticos/OperadoresAritmeticos.java
+++ src/main/java/com/mycompany/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -12,9 +12,28 @@ package com.mycompany.operadoresaritmeticos;
 public class OperadoresAritmeticos {
 
     public static void main(String[] args) {
-        int n1 = 3;
+        /*int n1 = 3;
         int n2 = 5;
         float m = (n1 + n2)/2;
-        System.out.println("A média é igual a " + m );
+        System.out.println("A média é igual a " + m ); */
+        
+        /*int numero =  10;
+        int valor = 4 + numero--;
+        System.out.println(valor);
+        System.out.println(numero);*/
+        
+        /*int x = 4;
+        x += 2; // x = x + 2
+        System.out.println(x);*/
+        
+        /*float v  = 8.5f;
+        int ar = (int) Math.round(v);
+        System.out.println(ar);*/
+        
+        double ale = Math.random();
+        int n = (int) (15 + ale * (50-15));
+        System.out.println(n);
+        
+        
     }
 }
