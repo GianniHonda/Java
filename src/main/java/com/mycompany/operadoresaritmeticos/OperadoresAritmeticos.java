ca479cd9797da0de6d8fb55d41a43140d66ef1ac
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-02-07 19:40:27 +0900

    Teste de operador aritmético média em JAVA

---
+++ src/main/java/com/mycompany/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.operadoresaritmeticos;
+
+/**
+ *
+ * @author gigi_
+ */
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é igual a " + m );
+    }
+}
