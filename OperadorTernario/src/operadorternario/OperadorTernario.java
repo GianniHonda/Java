bdee14031e0329da3d2b12c44d87b315b05916a7
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-02-21 19:05:48 +0900

    Teste de operador  ternário em JAVA

---
+++ OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadorternario;
+
+/**
+ *
+ * @author gigi_
+ */
+public class OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1, n2, r;
+        n1 = 4;
+        n2 = 8;
+        r =  (n1>n2)?0:1;
+        System.out.println(r);
+    }
+    
+}
