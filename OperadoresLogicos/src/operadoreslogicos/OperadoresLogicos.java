37e41d342791a66e9f1075f3ca233ec31bf5c265
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-02-21 20:26:49 +0900

    Operadores Lógicos em JAVA

---
+++ OperadoresLogicos/src/operadoreslogicos/OperadoresLogicos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoreslogicos;
+
+/**
+ *
+ * @author gigi_
+ */
+public class OperadoresLogicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x,  y, z;
+        x = 4;
+        y = 7;
+        z = 12;
+        boolean r;
+        r = (x<y && y==z)?true:false;
+        System.out.println(r);
+    }
+    
+}
