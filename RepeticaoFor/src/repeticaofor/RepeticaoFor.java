6893e8030843ab97f2429b11f7fdd880ce9314ce
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-04-30 14:21:05 +0900

    Contador com estrutura de repetição for

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author gigi_
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc=15; cc>=5; cc-=2) {
+            System.out.println(cc);
+        }
+    }
+    
+}
