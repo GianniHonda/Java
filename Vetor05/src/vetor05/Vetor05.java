29f5113126323a184888a7f274b0d7f03aa6b421
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-05-11 21:34:39 +0900

    Vetoração com fill

---
+++ Vetor05/src/vetor05/Vetor05.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor05;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author gigi_
+ */
+public class Vetor05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int v[] = new int [20];
+        Arrays.fill(v, 0);
+        for(int valor:v) {
+            System.out.print(valor + " ");
+        }
+        }
+    }
+    
+
