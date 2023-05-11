8441f5cd334830879495d3d676f542fcb39fd340
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-05-11 21:09:37 +0900

    Vetoração com for it

---
+++ Vetor03/src/vetor03/Vetor03.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor03;
+
+/**
+ *
+ * @author gigi_
+ */
+public class Vetor03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double v[] = {3.5, 2.75, 9, -4.5};
+        for (double valor: v){
+            System.out.print(valor + " ");
+        }
+    }
+    
+}
