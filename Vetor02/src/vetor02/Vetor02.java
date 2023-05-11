f1f9c6c7828ff3ef67cd65edf20a5827843860a0
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-05-11 21:01:33 +0900

    Quantidade de dias no mês com vetoração

---
+++ Vetor02/src/vetor02/Vetor02.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor02;
+
+/**
+ *
+ * @author gigi_
+ */
+public class Vetor02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String mes[] = {"Jan","Fev","Mar","Abr","Mai","Jun",
+        "Jul","Ago","Set","Out","Nov","Dez"};
+        int tot[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for(int c = 0; c<mes.length; c++){
+            System.out.println("O mês de " + mes[c] + " tem " + tot[c] + " dias ao todo.");
+        }
+    }
+    
+}
