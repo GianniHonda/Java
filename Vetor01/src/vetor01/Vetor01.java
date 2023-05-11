9a5cf223d5632852f7900f1c8d352d82b3a82198
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-05-11 20:50:24 +0900

    Vetores em JAVA

---
+++ Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor01;
+
+/**
+ *
+ * @author gigi_
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n[] = {3,2,8,7,5,4};
+        for(int c = 0; c<=n.length-1; c++){
+            System.out.println("na posição " + c + " temos o valor " +n[c]);
+            
+        } 
+    }
+    
+}
