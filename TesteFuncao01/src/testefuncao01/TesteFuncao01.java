a7416a21e000a875fd359ad3904007ee4066b802
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-05-19 19:01:07 +0900

    Métodos - função soma

---
+++ TesteFuncao01/src/testefuncao01/TesteFuncao01.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testefuncao01;
+
+/**
+ *
+ * @author gigi_
+ */
+public class TesteFuncao01 {
+    static void soma(int a, int b) {
+        int s = a + b;
+        System.out.println("A soma é " + s);}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Começou o programa");
+        soma(5,2);
+    }
+   
+    
+}
